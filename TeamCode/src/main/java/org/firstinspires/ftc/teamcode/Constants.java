5ed8b3e8bd9bda2313fdb362c2b830e6f615d527
lmcginnisno1 <lmcginnisno1@gmail.com>
2025-09-13 12:48:53 -0400

    Added lift subsystem and updated shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Constants {
-
     public static final class shooterConstants {
-        public static final double kKickPositionLeft = 100;
-        public static final double kKickPositionRight = 200;
-    }
+        public static final double shooterP = 0;
+        public static final double shooterD = 0;
+        public static final double shooterF = 0;
 
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SUB_Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SUB_Shooter.java
@@ -1,51 +1,63 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-
+import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.SubsystemBase;
-import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@Config
 public class SUB_Shooter extends SubsystemBase {
 
     private final OpMode m_OpMode;
     private final DcMotorEx m_shooterMotorLeft;
     private final DcMotorEx m_shooterMotorRight;
-    private final ServoEx m_kickerRight;
-    private final ServoEx m_kickerLeft;
+    private final CRServo m_kickerRight;
+    private final CRServo m_kickerLeft;
+
+    public static double shooterP = 0;
+    public static double shooterD = 0;
+    public static double shooterF = 0;
+    public static int shooterVelocity = 0;
+
+    private final boolean tuning = true;
+
 
     public SUB_Shooter(OpMode p_OpMode) {
 
         m_OpMode = p_OpMode;
         m_shooterMotorLeft = m_OpMode.hardwareMap.get(DcMotorEx.class, "shooterMotorLeft");
         m_shooterMotorRight = m_OpMode.hardwareMap.get(DcMotorEx.class, "shooterMotorRight");
-        m_kickerLeft = m_OpMode.hardwareMap.get(ServoEx.class, "kickerLeft");
-        m_kickerRight = m_OpMode.hardwareMap.get(ServoEx.class, "kickerRight");
+        m_kickerLeft = m_OpMode.hardwareMap.get(CRServo.class, "kickerLeft");
+        m_kickerRight = m_OpMode.hardwareMap.get(CRServo.class, "kickerRight");
         m_shooterMotorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         m_shooterMotorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
-    public void readyToShoot() {
-
-        m_shooterMotorRight.setPower(1);
-        m_shooterMotorLeft.setPower(1);
+    public void setVelocity(double velocity) {
+        m_shooterMotorLeft.setVelocity(velocity, AngleUnit.DEGREES);
+        m_shooterMotorRight.setVelocity(velocity, AngleUnit.DEGREES);
     }
 
-    public void kickRight(double pos) {
-
-        m_kickerRight.setPosition(pos/300);
+    public void kickRight(double power) {
+        m_kickerRight.setPower(power);
     }
 
-    public void kickLeft(double pos) {
-
-        m_kickerLeft.setPosition(pos/300);
+    public void kickLeft(double power) {
+        m_kickerLeft.setPower(power);
     }
 
     @Override
     public void periodic() {
-        telemetry.addData("kickerLeft", m_kickerLeft.getPosition());
-        telemetry.addData("kickerRight", m_kickerRight.getPosition());
+        if (tuning) {
+            m_shooterMotorLeft.setVelocityPIDFCoefficients(shooterP, 0, shooterD, shooterF);
+            m_shooterMotorRight.setVelocityPIDFCoefficients(shooterP, 0, shooterD, shooterF);
+
+            m_shooterMotorLeft.setVelocity(shooterVelocity, AngleUnit.DEGREES);
+            m_shooterMotorRight.setVelocity(shooterVelocity, AngleUnit.DEGREES);
+        }
     }
 }
